05a9ec468cc4c348aa789e89cb7b9ca2554c08fb
anjali rathore <rathoreanjali712@gmail.com>
2024-08-11 14:18:26 +0530

    this is my first commit

---
+++ first_project/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+ 
+
+  return (
+   
+     <>
+     <h1>hello react .....i'm anjali rathore from indore</h1>
+     </>
+  )
+}
+
+export default App
---
+++ first_project/src/main.jsx
@@ -0,0 +1,9 @@
+
+import App from './App.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+ 
+    <App />
+ 
+)
